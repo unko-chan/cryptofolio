6fa656c32447888c6d18fe5ea4f970a175440e69
unko-chan <kevinliang0303@gmail.com>
2021-03-15 10:20:23 -0700

    implement basic performance chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.scss';
 import SideNav from './Components/Nav/SideNav';
 import Doughnut from "./charts/doughnut";
+import VoteLine from "./charts/performanceChartLine"
 
 function App() {
   return (
-  <div className="App" >
-    <header className="App-header">
-       <div className="main-container">
-        <div className="side-nav">
-          <SideNav />
+    <div className="App">
+      <header className="App-header">
+        <div className="main-container">
+          <div className="side-nav">
+            <SideNav />
+          </div>
         </div>
-      </div>
-    <div class="chart-container">
-      <Doughnut />
-    </div>
+        <div class="chart-container">
+          <VoteLine />
+
+          <Doughnut />
+        </div>
       </header>
-  </div>
-
-
-)
+    </div>
+  );
   }
 
 export default App;
