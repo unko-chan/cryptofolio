a815d399f084d595d7e60ed56cea7c4a5ee62ffe
Taylor <ivingstaylor@gmail.com>
2021-03-14 22:25:27 -0700

    material ui icons add to navbar

--- src/Components/Nav/SideNav.js
+++ src/Components/Nav/SideNav.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import './Nav.scss'
+import AccountBalanceWalletOutlinedIcon from '@material-ui/icons/AccountBalanceWalletOutlined';
+import TrendingUpIcon from '@material-ui/icons/TrendingUpOutlined';
+import ImportContactsOutlinedIcon from '@material-ui/icons/ImportContactsOutlined';
+import AttachMoneyOutlinedIcon from '@material-ui/icons/AttachMoneyOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import logo from './C_icon_without_white.png';
+
+import './Nav.scss'
+
 
 
 const SideNav = (props) => {
@@ -11,34 +18,34 @@ const SideNav = (props) => {
 
       <div className="nav-links">
       <div className="hover-to-show-link">
-        <a href="#section" className="nav-header">Dashboard</a>
+        <a href="#section" className="nav-header"> <AccountBalanceWalletOutlinedIcon /> Dashboard</a>
         <span className="nav-subtitle hover-to-show">View your wallet</span>
       </div>
 
       <div className="hover-to-show-link">
-        <a href="#section" className="nav-header">Market</a>
+        <a href="#section" className="nav-header"><TrendingUpIcon /> Market</a>
         <span className="nav-subtitle hover-to-show">View market charts</span>
       </div>
 
       <div className="hover-to-show-link">
-        <a href="#section" className="nav-header">News</a>
+        <a href="#section" className="nav-header"><ImportContactsOutlinedIcon /> News</a>
         <span className="nav-subtitle hover-to-show">The latest crypto news</span>
       </div>
 
       <div className="hover-to-show-link">
-        <a href="#section" className="nav-header">Exchange</a>
+        <a href="#section" className="nav-header"><AttachMoneyOutlinedIcon />  Exchange</a>
         <span className="nav-subtitle hover-to-show">Crypto exchange</span>
 
       </div>
 
       <div className="hover-to-show-link">
-        <a href="#section" className="nav-header">Settings</a>
+        <a href="#section" className="nav-header"><SettingsOutlinedIcon /> Settings</a>
         <span className="nav-subtitle hover-to-show">User settings</span>
       </div>
       </div> 
 
       <span className="nav-logo">
-       <img src={logo} style={{width: 200, height: 200 }}/> 
+       <img src={logo} style={{width: 200, height: 200 }} alt={""}/> 
        </span>
     </div>
   )
