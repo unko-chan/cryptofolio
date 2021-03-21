36684a486e72fa69a98719eb219b460d0fdf4fd6
Taylor <ivingstaylor@gmail.com>
2021-03-20 19:08:49 -0700

    fix render bug

--- src/Components/news/ArticleListItem.js
+++ src/Components/news/ArticleListItem.js
@@ -3,9 +3,8 @@ import { Paper, Box } from '@material-ui/core'
 
 
 export default function ArticleListItem(props) {
-
   return (
-    <Box component={Paper} elevation={3}>
+    <div>
       <div className="article-name">{props.name}</div>
     <div className="article-author">Written by: {formatAuthor(props.author)}</div><br/>
     <div className="article-description">{props.description}</div>
@@ -14,11 +13,12 @@ export default function ArticleListItem(props) {
       e.preventDefault();
       window.open(props.url);
       }}>Click for full story</a>
-      </Box>
+      </div>
   );
 }
 
 const formatAuthor = (author) => {
+  console.log('is it running')
   if (!author) {
     return 'Anonymous'
   } else {
